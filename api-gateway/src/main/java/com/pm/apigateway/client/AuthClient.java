0f9ea8102b50ce857ba9472d2a154761810eeabc
markcus <markcus0526@proton.me>
2026-03-03 21:09:41 +0100

    Update docker-compose

--- api-gateway/src/main/java/com/pm/apigateway/client/AuthClient.java
+++ api-gateway/src/main/java/com/pm/apigateway/client/AuthClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service-default")
+@FeignClient(name = "AUTH-SERVICE", url = "${auth-service.url}")
 public interface AuthClient {
     @GetMapping("/validate")
     void validateToken(@RequestHeader("Authorization") String token);
