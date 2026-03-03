e43744fd95048b873dbfc308c45970c48ccc0202
markcus <markcus0526@proton.me>
2026-03-03 08:54:55 +0100

    Update api-gateway with balancer

--- api-gateway/src/main/java/com/pm/apigateway/client/AuthClient.java
+++ api-gateway/src/main/java/com/pm/apigateway/client/AuthClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth-service-default")
 public interface AuthClient {
     @GetMapping("/validate")
     void validateToken(@RequestHeader("Authorization") String token);
