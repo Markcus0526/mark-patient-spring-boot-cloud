58bacf73a223b3c593c6908c47b48618595ff685
markcus <markcus0526@proton.me>
2026-03-05 03:17:09 +0100

    Update all modules

--- patient-service/src/main/java/com/pm/patientservice/config/RabbitMQConfig.java
+++ patient-service/src/main/java/com/pm/patientservice/config/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package com.pm.patientservice.config;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
